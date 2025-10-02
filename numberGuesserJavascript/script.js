076f8c1ab77dff367c84fed4a3c138d5120df0eb
Justin-Ebenazar <thinkgoodone03@gmail.com>
2025-10-02 12:05:12 +0530

    Random num gen , Number guesser

---
+++ numberGuesserJavascript/script.js
@@ -0,0 +1,37 @@
+let guess;
+
+function guessFix(){
+    const min = Number(document.getElementById("min").value);
+    const max = Number(document.getElementById("max").value);
+    const guessButt = document.getElementById("guess");
+    const userGuess = document.getElementById("myGuess");
+    guessButt.disabled = false;
+    userGuess.disabled = false;
+    
+    guess = Math.floor(Math.random() * (max - min)) + min;
+    alert("You can start playing now!");
+}
+
+function guessCheck(){
+    const userGuess = Number(document.getElementById("myGuess").value);
+    const guessy = document.getElementById("Guess-answer");
+    const wish = document.getElementById("wish");
+    const guessButt = document.getElementById("guess");
+    
+    if(userGuess === guess){
+        guessy.innerHTML = ("");
+        wish.innerHTML = ("You guessed it right!");
+        userGuess.disabled = false;
+        guessButt.disabled = false;
+    } else if(userGuess < guess){
+        guessy.innerHTML = ("");
+        setTimeout(() => {
+            guessy.innerHTML = ("Too low! Try again.");}, 200);
+        } 
+    else {
+        guessy.innerHTML = ("");
+        setTimeout(() => {
+        guessy.innerHTML = ("Too high! Try again.");}, 200);
+    }   
+    // document.getElementById("myGuess").value = "";
+}
